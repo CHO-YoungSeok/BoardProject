ecc1e921372cd58fc7ed629c8db1a5dd9ef7906f
cys <dudtjr309@hansung.ac.kr>
2024-12-20 00:57:18 +0900

    update basic frame

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const port = 8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', __dirname + '/views');
+
 
 app.listen(port, ()=> {
-    console.log(port + "port watting....");
+    console.log(`${port} 에서 대기중...`);
+})
+
+app.get('/', (req, res) => {
+    res.render('main');
+})
+app.get('/center', (req, res) => {
+    res.render('center');
+})
+app.get('/footer', (req, res) => {
+    res.render('footer');
 })
 
