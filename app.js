d43f369be72825cedeb8adc31749dbc689f77c28
cys <dudtjr309@hansung.ac.kr>
2024-12-21 21:40:16 +0900

    update mysql but its not complete

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+let db_info = {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "1234",
+    database: "board"
+};
+let db_table = "contents";
+const mysql = require('mysql2');
+let conn = mysql.createConnection(db_info);
+conn.connect;
 
 app.listen(port, ()=> {
     console.log(`${port} 에서 대기중...`);
@@ -20,4 +31,14 @@ app.get('/center', (req, res) => {
 app.get('/footer', (req, res) => {
     res.render('footer');
 })
-
+app.get('/list', (req, res) => {
+    let sql = `SELECT * FROM ${db_table} ORDERBY no DESC`;
+    conn.query(sql, (err, result) => {
+        if(err) {
+            console.log("ERROR: query is not excute from list\n" + err );
+        }
+        else {
+            res.render('list', {data:result});
+        }
+    })
+})
