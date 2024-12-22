e7cd82b6db8b454fcda719542c47b3bb510f2725
cys <dudtjr309@hansung.ac.kr>
2024-12-22 16:30:34 +0900

    update list.ejs but not complete

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # BoardProject
 
+### 2024.12.18 작성함함
 ## 구현할 기능
 - 게시글 올리기
 - 목록 리스트 및 정렬
@@ -9,13 +10,22 @@
 ## 스택
 - nodejs
 - html, css, js, ejs
-- gitHub
-- gitHub Action
-- aws
+- VCS: gitHub
+
+## 개발 기간
+- 1주일 (2024.12.18 ~ 2024.12.25)
 
 ## 프로젝트 목표
 - nodejs 숙련도 키우기
 - gitHub, gitHub action, aws등 프로젝트 툴 배우기
+
+
+
+## 추가 예정
+
+- aws: 배포
+- gitHub Action : 테스트 자동화 및 자동 배포
+
 
 
 
--- app.js
+++ app.js
@@ -13,10 +13,11 @@ let db_info = {
     password: "1234",
     database: "board"
 };
+
 let db_table = "contents";
 const mysql = require('mysql2');
 let conn = mysql.createConnection(db_info);
-conn.connect;
+conn.connect();
 
 app.listen(port, ()=> {
     console.log(`${port} 에서 대기중...`);
@@ -32,13 +33,13 @@ app.get('/footer', (req, res) => {
     res.render('footer');
 })
 app.get('/list', (req, res) => {
-    let sql = `SELECT * FROM ${db_table} ORDERBY no DESC`;
+    let sql = "SELECT * FROM contents";
     conn.query(sql, (err, result) => {
         if(err) {
-            console.log("ERROR: query is not excute from list\n" + err );
+            console.log(err);
         }
         else {
-            res.render('list', {data:result});
+            res.render("list", { data:result });
         }
     })
 })
